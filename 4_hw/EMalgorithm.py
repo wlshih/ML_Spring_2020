a6deb85dd83764d5f1c8a8b153fa6a2d3df28838
wlshih <wl.shih2010@gmail.com>
2020-05-05 03:22:12 +0800

    modify em algo, bug not fixed

--- 4_hw/EMalgorithm.py
+++ 4_hw/EMalgorithm.py
@@ -10,110 +10,112 @@ fnLabel = "train-labels-idx1-ubyte"
 def read_data():
 	data_type = np.dtype("int32").newbyteorder('>')  # big endian
 
-	data = np.fromfile(fnImage, dtype = 'ubyte')
-	img_bin = data[4 * data_type.itemsize:].astype("float64").reshape(60000, 28 * 28).transpose()
-	img_bin = np.divide(img_bin, 128).astype("int")
+	data = np.fromfile(fnImage, dtype = 'ubyte').astype("float64")
+	data = data[4 * data_type.itemsize : ].reshape(60000, 28 * 28).transpose()  # ignore 16-byte headers
+	data = np.divide(data, 128).astype("int")
 
 	label = np.fromfile(fnLabel, dtype = 'ubyte').astype('int')
-	label = label[2 * data_type.itemsize : ].reshape(60000)
+	label = label[2 * data_type.itemsize : ].reshape(60000)  # ignore 8-byte headers
 	
-	return img_bin, label
+	return data, label
 
 @jit
-def E_step(img_bin, MU, PI, Z):
+def E_step(data, lamb, P):
+	# k labels, n training data sets
+	Z = np.zeros((10, 60000), dtype=np.float64)
 	for n in range(60000):
-		tmp = np.zeros(shape=(10), dtype=np.float64)
+		#print(n)
+		marginal = 0  # denominator
 		for k in range(10):
-			tmp1 = np.float64(1.0)
+			p = lamb[k]
 			for i in range(28*28):
-				if img_bin[i][n]:
-					tmp1 *= MU[i][k]
+				if data[n][i]:
+					p *= P[i][k]
 				else:
-					tmp1 *= (1 - MU[i][k])
-			tmp[k] = PI[k][0] * tmp1
-		tmp2 = np.sum(tmp)
-		if tmp2 == 0:
-			tmp2 = 1
+					p *= (1 - P[i][k])
+			Z[k][n] = p 
+			marginal += p
+
+		if marginal == 0:
+			marginal = 1
 		for k in range(10):
-			Z[k][n] = tmp[k] / tmp2
+			Z[k][n] /= marginal
 	
 	return Z
 
-@jit
-def M_step(img_bin, MU, PI, Z):
-	N = np.sum(Z, axis=1)
-	for j in range(28*28):
-		for m in range(10):
-			tmp = np.dot(img_bin[j], Z[m])
-			tmp1 = N[m]
-			if tmp1 == 0:
-				tmp1 = 1
-			MU[j][m] = tmp / tmp1
-	
-	for i in range(10):
-		PI[i][0] = N[i] / 60000
-	
-	return MU, PI
-	
 
-# print image
-def print_MU(MU):
-	print("MU size", len(MU), len(MU[0]))
-	for i in range(10):
-		print("\nclass: ", i)
-		for j in range(28*28):
-			if j % 28 == 0 and j != 0:
+@jit
+def M_step(data, lamb, P, Z):
+	for k in range(10):
+		N = np.sum(Z[k])  # N = sigma(Zm) = total 1's occurs in class m
+		lamb[k] = N / 60000
+		if N == 0:
+			N = 1
+		for i in range(28 * 28):
+			P[i][k] = np.sum(np.matmul(Z[k], data[:][i])) / N
+
+	return lamb, P
+			
+
+# print imagine of number
+def print_imagin(P):
+	for k in range(10):
+		print("\nclass: ", k)
+		for i in range(28*28):
+			if i % 28 == 0 and i != 0:
 				print("")
-			if MU.T[i][j] >= 0.5:
+			if P[i][k] >= 0.5:
 				print(" ", end=' ')
 			else:
 				print("0", end=' ')
 		print("")
 
 
-@jit
+#@jit
 def EM_algorithm():
-	img_bin, label = read_data()
 
-	PI = np.full((10, 1), 0.1, dtype=np.float64)
-	MU = np.random.rand(28 * 28, 10).astype(np.float64)
-	MU_prev = np.zeros((28 * 28, 10), dtype=np.float64)
-	Z = np.full((10, 60000), 0.1, dtype=np.float64)
+	print("---(read data)---")
+	data, label = read_data()
+	print("\n  train data size: {}\n".format(data.shape))
+
+
+	P = np.random.rand(28 * 28, 10).astype(np.float64)    # probability of each bit of each class
+	Z = np.full((10, 60000), 0.1, dtype=np.float64)       # responsibility
+	lamb = np.full(10, 0.1, dtype=np.float64)             # lambda, mean MLE of each class
+	lamb_prev = np.zeros(10, dtype=np.float64)            # lambda from last iteration, for convergence test
+
 
 	cond = 0
-	it = 1
+	it = 0 
 	while(True):
 		it += 1
 
 		# E step, get Z
-		print("---(E step)---")
-		Z = E_step(img_bin, MU, PI, Z)
+		print("---(E step {})---".format(it))
+		Z = E_step(data, lamb, P)
 
-		# M step, get MU, PI
-		print("---(M step)---")
-		MU, PI = M_step(img_bin, MU, PI, Z)
+		# M step, get lamb, P
+		print("---(M step {})---".format(it))
+		lamb, P = M_step(data, lamb, P, Z)
 
 		# condition check
-		flag = 0
-		for i in range(10):
-			if PI[i][0] == 0:
-				cond = 0
-				flag = 1
-				PI = np.full((10, 1), 0.1, dtype=np.float64)
-				MU = np.random.rand(28 * 28, 10).astype(np.float64)
-				Z = np.full((10, 60000), 0.1, dtype=np.float64)
-				break
-		
-		if flag == 0:
+		if 0 in lamb:
+			cond = 0
+			# initialize lambda and P if lambda == 0 
+			P = np.random.rand(28 * 28, 10).astype(np.float64)
+			lamb = np.full(10, 0.1, dtype=np.float64)
+			print("---(restart)---")
+		else:
 			cond += 1
 
-		if np.sum(abs(MU - MU_prev)) < 20 and cond >= 8 and np.sum(PI) > 0.95:
+		diff = np.sum(abs(lamb - lamb_prev))
+		if diff < 5e-3 and cond >= 8 and np.sum(P) > 0.95:
 			break
-			
-		MU_prev = MU
+		
+		lamb_prev = np.copy(lamb)
 
-		print_MU(MU)
-		print("No. of Iteration: {}, Difference: {}\n".format(it, np.sum(abs(MU - MU_prev))))
+		print_imagin(P)
+		print("No. of Iteration: {}, Difference: {}\n".format(it, diff))
 		print("---------------------------------------------------------------------------\n")
 	
 	print("---------------------------------------------------------------\n")
