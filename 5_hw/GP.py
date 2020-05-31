694df99b4bd180d9e5d07dda9a262f9fa717a545
wlshih <wl.shih2010@gmail.com>
2020-05-31 20:31:52 +0800

    optimized kernel parameter by minimizing nll

--- 5_hw/GP.py
+++ 5_hw/GP.py
@@ -23,45 +23,71 @@ def kernel(X1, X2, sigma, alpha, length_scale):
 	return (sigma ** 2) * (1 + (cdist(X1, X2, 'sqeuclidean') / (2 * alpha * (length_scale ** 2)))) ** (-alpha)
 
 
-def GaussainProcess(X, Y, beta, sigma, alpha, length_scale):
+def GaussianProcess(X, Y, beta, sigma, alpha, length_scale, optimized):
 	mu = np.zeros(X.shape)
 	cov = kernel(X, X, sigma, alpha, length_scale) + 1 / beta * np.identity(X.shape[0])
 	cov_inv = np.linalg.inv(cov)
 
 	# test
-	X_test = np.linspace(-60, 60, 1000).reshape(-1, 1)
+	X_test = np.linspace(-60, 60, 120).reshape(-1, 1)
 
 	cov_test = kernel(X_test, X_test, sigma, alpha, length_scale) + 1 / beta
 	cov_train_test = kernel(X, X_test, sigma, alpha, length_scale)
 
-	Y_test = np.linalg.multi_dot([cov_train_test.T, cov_inv, Y])
-	std = np.sqrt(cov_test - np.linalg.multi_dot([cov_train_test.T, cov_inv, cov_train_test]))
+	Y_test = cov_train_test.T @ cov_inv @ Y
+	std = np.sqrt(cov_test - cov_train_test.T @ cov_inv @ cov_train_test)
+	# 95% confidence interval: 2 * std
 	Y_test_plus = Y_test + 2 * (np.diag(std).reshape(-1, 1))
 	Y_test_minus = Y_test - 2 * (np.diag(std).reshape(-1, 1))
 
-	print(Y_test_plus)
-	print(Y_test_minus)
+	#print(Y_test_plus)
+	#print(Y_test_minus)
 	
 	# graph
-	plt.title("Gaussian Process with length-scale : {0:.2f} sigma : {1:.2f} alpha : {2:.2f}".format(length_scale, sigma, alpha))
-	plt.scatter(X, Y, color='black')
-	plt.plot(X_test, Y_test, color='blue')
-	plt.plot(X_test, Y_test_plus, color='red')
-	plt.plot(X_test, Y_test_minus, color='red')
+	if optimized:
+		plt.title("Optimized Gaussian Process\nlength-scale : {0:.2f} sigma : {1:.2f} alpha : {2:.2f}".format(length_scale, sigma, alpha))
+	else:
+		plt.title("Gaussian Process\nlength-scale : {0:.2f} sigma : {1:.2f} alpha : {2:.2f}".format(length_scale, sigma, alpha))
+	plt.plot(X_test, Y_test, color='b')
+	plt.plot(X_test, Y_test_plus, color='r')
+	plt.plot(X_test, Y_test_minus, color='r')
+	plt.fill_between(X_test.ravel(), Y_test_plus.ravel(), Y_test_minus.ravel(), facecolor='pink')
+	plt.scatter(X, Y, color='k')
 
 	plt.xlim(-60, 60)
 	plt.show()
 
+def negLogLikelihood(pars, X, Y, beta):
+	kern = kernel(X, X, pars[0], pars[1], pars[2])
+	kern += np.identity(len(X), dtype=np.float64) * (1 / beta)
+
+	nll = np.sum(np.log(np.diagonal(np.linalg.cholesky(kern))))
+	nll += 0.5 * Y.T @ np.linalg.inv(kern) @ Y
+	nll += 0.5 * len(X) * np.log(2 * np.pi)
+
+	return nll
 
 
 if __name__ == "__main__":
 
 	X, Y = readData()
 
-	'''
 	beta = 5
 	sigma = 1
 	alpha = 1
 	length_scale = 1
-	'''
-	GaussainProcess(X, Y, 5, 1, 1, 1)
+
+	GaussianProcess(X, Y, beta, sigma, alpha, length_scale, 0)
+
+	# hyperparameter optimization
+	sigma = 1
+	alpha = 1
+	length_scale = 1
+
+	opt = minimize(negLogLikelihood, [sigma, alpha, length_scale], bounds=((1e-8, 1e6), (1e-8, 1e6), (1e-8, 1e6)), args=(X, Y, beta))
+	sigma_opt = opt.x[0]
+	alpha_opt = opt.x[1]
+	length_scale_opt = opt.x[2]
+
+	GaussianProcess(X, Y, beta, sigma_opt, alpha_opt, length_scale_opt, 1)
+
